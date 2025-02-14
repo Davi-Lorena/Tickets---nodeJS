689fd175e76ecad22e68b7567a62e98420afa571
Davi Lorena <davilorena2016@gmail.com>
2025-02-14 09:53:36 -0300

    updating create.js

--- src/Controller/tickets/create.js
+++ src/Controller/tickets/create.js
@@ -1,4 +1,21 @@
+import { randomUUID } from "node:crypto"
+
 export function create ({ request, response }){
+
+    const { equipment, description, user_name } = request.body
+
+    const ticket = {
+        id: randomUUID(),
+        equipment,
+        description,
+        user_name,
+status: "open",
+        created_at: new Date(),
+        updated_at: new Date()
+    }
+
+
+
     response.writeHead(201)
     response.end("Deu bom fml")
 }
