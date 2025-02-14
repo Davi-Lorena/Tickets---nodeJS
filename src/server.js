9bc75f3d286db98ac07b8d66c95a1f63e1aa46fa
Davi Lorena <davilorena2016@gmail.com>
2025-02-14 09:40:16 -0300

    adding routeHandler

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import http from "node:http"
 
 import { jsonHandler} from "./middlewares/jsonHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
 
 async function listener(request, response) {
 // Função que será executada quando o servidor receber uma requisição
 await jsonHandler(request, response)
+routeHandler(request, response)
 
 }
 
